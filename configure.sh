530b4426b05310b2f93f022b138438834713bb47
harper <ukulililixl@gmail.com>
2015-09-26 11:18:27 +0000

    second

--- configure.sh
+++ configure.sh
@@ -3,10 +3,10 @@
 site=/home/hadoop/hadoop/conf
 
 sed -i '6a\
-\t<property>\n\t\t<name>hadoop.tmp.dir</name>\n\t\t<value>/home/hadoop/hadoop/tmp</value>\n\t</property>\n\t<property>\n\t\t<name>fs.default.name</name>\n\t\t<value>hdfs://vm1:54310</value>\n\t</property>' $site/core-site.xml
+\t<property>\n\t\t<name>hadoop.tmp.dir</name>\n\t\t<value>/home/hadoop/hadoop/tmp</value>\n\t</property>\n\t<property>\n\t\t<name>fs.default.name</name>\n\t\t<value>hdfs://lxl1:54310</value>\n\t</property>' $site/core-site.xml
 
 sed -i '6a\
-\t<property>\n\t\t<name>mapred.job.tracker</name>\n\t\t<value>vm1:54311</value>\n\t</property>' $site/mapred-site.xml
+\t<property>\n\t\t<name>mapred.job.tracker</name>\n\t\t<value>lxl1:54311</value>\n\t</property>' $site/mapred-site.xml
 
 sed -i '6a\
 \t<property>\n\t\t<name>dfs.replication</name>\n\t\t<value>3</value>\n\t</property>' $site/hdfs-site.xml
