c912c5d5aeaa149ba14b498f9240ac4cb09c218b
harper <ukulililixl@gmail.com>
2015-09-26 11:28:03 +0000

    four

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#wget http://archive.apache.org/dist/hadoop/core/hadoop-0.20.203.0/hadoop-0.20.203.0rc1.tar.gz
-#tar xzf hadoop-0.20.203.0rc1.tar.gz
-#mv hadoop-0.20.203.0 hadoop
+wget http://archive.apache.org/dist/hadoop/core/hadoop-0.20.203.0/hadoop-0.20.203.0rc1.tar.gz
+tar xzf hadoop-0.20.203.0rc1.tar.gz
+mv hadoop-0.20.203.0 hadoop
 
 now=`pwd`
-#echo 'export HADOOP_HOME=/home/hadoop/hadoop' >> $now/.bashrc
-#echo 'export PATH=$PATH:$HADOOP_HOME/bin' >> $now/.bashrc
+echo 'export HADOOP_HOME=/home/hadoop/hadoop' >> $now/.bashrc
+echo 'export PATH=$PATH:$HADOOP_HOME/bin' >> $now/.bashrc
 echo 'export PATH=$PATH:/sbin' >> $now/.bashrc
